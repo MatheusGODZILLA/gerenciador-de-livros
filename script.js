48fd2ba09cbaf13601d405eab62fbbe275fbd96c
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-06-29 04:52:19 -0300

    Criando a função para cadastrar um livro

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ const nullMessage = document.getElementById('null');
 
 // Array para armazenar os livros
 let library = [];
+
+// Função para cadastrar um livro
+function cadastrarLivro(event) {
+    event.preventDefault();
+  
+    // Obter os valores dos campos de entrada
+    const title = titleInput.value;
+    const author = authorInput.value;
+    const publicationDate = publicationDateInput.value;
+  
+    // Criar um objeto de livro
+    const book = { title, author, publicationDate };
+  
+    // Adicionar o livro ao array de livros
+    library.push(book);
+  
+    // Limpar os campos de entrada
+    titleInput.value = '';
+    authorInput.value = '';
+    publicationDateInput.value = '';
+  
+    // Atualizar a lista de livros
+    atualizarListaLivros();
+  
+    // Salvar os livros no Local Storage
+    saveLocalStorage();
+  }
